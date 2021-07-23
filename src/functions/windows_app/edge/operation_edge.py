5933208d26b762560d22d9bc2f30686406c742b5
Ryuta Uematsu <r.resolute.pro@gmali.com>
2021-07-23 21:02:47 +0900

    コマンドでEdgeを起動できるように

--- src/functions/windows_app/edge/operation_edge.py
+++ src/functions/windows_app/edge/operation_edge.py
@@ -1,9 +1,8 @@
 from functions.windows_app.edge.edge_app import Edge
 from settings import EDGE_PATH
 
-WINDOWN_TITLE = 'Edge'
 
 app = Edge()
 
 def start_edge():
-    app.start_app(EDGE_PATH, WINDOWN_TITLE)
+    app.start_app(EDGE_PATH)
--- src/functions/windows_app/win_app.py
+++ src/functions/windows_app/win_app.py
@@ -6,14 +6,9 @@ class WindowsApplication():
     def __init__(self):
         pass
 
-    def start_app(self, path: str, window_title: str):
+    def start_app(self, path: str):
         # 渡されたプログラムを実行スタートする
         subprocess.Popen(r'{}'.format(path))
-        time.sleep(0.2)
-        # 最新に開いたwindowをactivate
-        print(gw.getAllTitles())
-        target = gw.getWindowsWithTitle(window_title)[0]
-        target.activate()
 
     #def close_app(self):
     #    pass
