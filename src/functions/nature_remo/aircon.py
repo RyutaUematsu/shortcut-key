d1d387434ca394b92be0aac94bca1ae18b04b3db
Ryuta Uematsu <r.resolute.pro@gmali.com>
2021-07-23 19:50:59 +0900

    ADD: エアコンの風量を変更できるように

--- src/functions/nature_remo/aircon.py
+++ src/functions/nature_remo/aircon.py
@@ -2,7 +2,13 @@ from functions.nature_remo.nature_remo_api import getAppliances, AIRCON_NAME, ap
 
 
 def aircon_on():
-    api.update_aircon_settings(getAppliances(api)[AIRCON_NAME].id,button='power-on')
+    api.update_aircon_settings(getAppliances(api)[AIRCON_NAME].id, button='power-on')
 
 def aircon_off():
-    api.update_aircon_settings(getAppliances(api)[AIRCON_NAME].id,button='power-off')
+    api.update_aircon_settings(getAppliances(api)[AIRCON_NAME].id, button='power-off')
+
+def aircon_volume_1():
+    api.update_aircon_settings(getAppliances(api)[AIRCON_NAME].id, air_volume="1")
+
+def aircon_volume_3():
+    api.update_aircon_settings(getAppliances(api)[AIRCON_NAME].id, air_volume="3")
